ebfd4d6ebcffd5bb21a1a7845f9de43949c0f2f1
bigswervbigswervNafismurray2002 <nafismurray.code@gmail.com>
2019-11-11 12:45:14 -0500

    created a seperate dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { summaryFileName } from '@angular/compiler/src/aot/util';
 import { GameSummaryComponent } from './game-summary-bar/game-summary-bar.component';
 import { LessonPlanDashboardComponent }  from './lesson-plan-dashboard/lesson-plan-dashboard.component'
+import { GamePageComponent } from './game-page/game-page.component';
 
 const routes: Routes = [
   { path: 'game-summary', component: GameSummaryComponent  },
   { path: 'lesson-plan-dashboard', component: LessonPlanDashboardComponent  },
+  { path: 'game-page', component: GamePageComponent  },
   { path: '', redirectTo: '/game-summary', pathMatch: 'full'},
 ];
 
