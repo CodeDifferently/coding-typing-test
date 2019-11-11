df664306fcaff2cc0ab8e290014a0a72d3bccbce
bigswervbigswervNafismurray2002 <nafismurray.code@gmail.com>
2019-11-11 16:50:48 -0500

    resolved problem

--- src/app/small-action-card/small-action-card.component.ts
+++ src/app/small-action-card/small-action-card.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { GameCollectionItem } from 'src/app/game-collection-item';
 
 @Component({
-  selector: 'app-small-action-card',
+  selector: 'small-action-card',
   templateUrl: './small-action-card.component.html',
   styleUrls: ['./small-action-card.component.css']
 })
